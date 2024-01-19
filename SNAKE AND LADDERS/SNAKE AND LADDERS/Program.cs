19b6e14a2f285b96a686290f70a41300e9ea10b2
Dhanush <dhanushdybala10@gmail.com>
2024-01-20 00:21:44 +0530

    [Dhanush] Add: Use Case 4 Program

--- SNAKE AND LADDERS/SNAKE AND LADDERS/Program.cs
+++ SNAKE AND LADDERS/SNAKE AND LADDERS/Program.cs
@@ -63,7 +63,7 @@ namespace SNAKE_AND_LADDERS
             HashSet<int> set = new HashSet<int>();
             for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < 7; j++)
                 {
                     int val;
                     if (i == 0)
@@ -100,32 +100,96 @@ namespace SNAKE_AND_LADDERS
             }
             return ladder_snake_noplay;
         }
+        public int[] snake_array(int[,] list)
+        {
+            int[] snake = new int[7];
+            for (int i = 0; i < list.GetLength(1); i++)
+            {
+                snake[i] = list[0, i];
+            }
+            return snake;
+        }
+        public int[] ladder_array(int[,] list)
+        {
+            int[] ladder = new int[7];
+            for (int i = 0; i < list.GetLength(1); i++)
+            {
+                ladder[i] = list[1, i];
+            }
+            return ladder;
+        }
+        public int[] noplay_array(int[,] list)
+        {
+            int[] noplay = new int[7];
+            for (int i = 0; i < list.GetLength(1); i++)
+            {
+                noplay[i] = list[0, i];
+            }
+            return noplay;
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
             Random random = new Random();
-            int player_position = 0;
-            string str;
+            int player_position = 1;
+            int chance = 0;
+            Console.WriteLine($"Player Position : {player_position}");
             dice die = new dice();
             int[,] ladder_snake_noplay=die.Ladder_Snake_Noplay();
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.Write($"{ladder_snake_noplay[i, j]} ");
-                }
-                Console.WriteLine();
-            }
+            int[] snake = die.snake_array(ladder_snake_noplay);
+            int[] ladder = die.ladder_array(ladder_snake_noplay);
+            int[] noplay = die.noplay_array(ladder_snake_noplay);
             do
             {
-                
+                Console.ReadKey();
                 int val = random.Next(6) + 1;
                 die.roll(val);
-                Console.WriteLine("Do you want to play ?");
-                str = Console.ReadLine();
-            } while (str == "y");
+                if (chance == 0)
+                player_position += val;
+                else 
+                    chance= 0;
+                Console.WriteLine($"Player Position : {player_position}");
+                if (snake.Contains(player_position))
+                {
+                    int snake_container;
+                    do
+                    {
+                        snake_container = random.Next(10, 30);
+                    } while (player_position - snake_container < 0);
+                    Console.WriteLine(snake_container);
+                    player_position -= snake_container;
+                    Console.WriteLine($"Snake : {snake_container}");
+                    Console.WriteLine(" ( ) ");
+                    Console.WriteLine("  \\  ");
+                    Console.WriteLine("  /  ");
+                    Console.WriteLine("  \\  ");
+                    Console.WriteLine("   / ");
+                    Console.WriteLine($"Player Position : {player_position}");
+                }
+                else if (ladder.Contains(player_position))
+                {
+                    int ladder_container;
+                    do
+                    {
+                        ladder_container = random.Next(10, 30);
+                    } while (player_position + ladder_container > 100);
+                    player_position += ladder_container;
+                    Console.WriteLine($"Ladder : {ladder_container}");
+                    Console.WriteLine("|-|  ");
+                    Console.WriteLine("|-|");
+                    Console.WriteLine("|-|");
+                    Console.WriteLine("|-|");
+                    Console.WriteLine("|-|");
+                    Console.WriteLine($"Player Position : {player_position}");
+                }
+                else if (noplay.Contains(player_position))
+                {
+                    chance = 1;
+                    Console.WriteLine($"Chance : {chance}");
+                }
+            } while (player_position < 100);
         }
     }
 }
